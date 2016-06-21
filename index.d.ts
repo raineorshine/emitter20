299b718b68fdcaf654a2654a93bc33c887fc00f2
Raine Revere <raineorshine@gmail.com>
2016-06-21 11:15:08 -0600

    Fix typings.

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
-declare module 'emitter20' {
-  interface Emitter {
-    on: (eventName: string, args: any) => void;
-    trigger: (eventName: string) => void;
-  }
-  function EmitterFactory(): Emitter;
-  export = EmitterFactory;
+declare class Emitter {
+  on: (eventName: string, cb: (data?: any) => void) => void;
+  trigger: (eventName: string, data?: any) => void;
 }
+
+export = Emitter;
